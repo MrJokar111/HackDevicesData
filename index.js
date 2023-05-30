1207b4be89c15dd057153ea77ebedbc4cfc759e2
MrJokar111 <87017746+MrJokar111@users.noreply.github.com>
2023-05-30 16:13:54 +0300

    index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.set("view engine", "ejs");
 
 //Modify your URL here
-var hostURL="https://demo.coolify.io/";
+var hostURL="http://cliaakqdp000040652cwlyrxi.demo.coolify.io";
 //TOGGLE for Shorters
 var use1pt=false;
 
